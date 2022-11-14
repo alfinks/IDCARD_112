97320d3008e2c31e3f777f514ce3ef5ce7aab091
alfinks <maulanaafiansyah@gmail.com>
2022-11-14 16:55:49 +0700

    Membuat Projek ID Card

---
+++ src/main/java/idcart/tugas_idcard/Idcard112Application.java
@@ -0,0 +1,13 @@
+package idcart.tugas_idcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idcard112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idcard112Application.class, args);
+	}
+
+}
---
+++ src/test/java/idcart/tugas_idcard/Idcard112ApplicationTests.java
@@ -0,0 +1,13 @@
+package idcart.tugas_idcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idcard112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
